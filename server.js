d61ad881c361fdcd1760030efe3b53d2e9fd9ed8
butterbaby4427 <aet395@nyu.edu>
2021-04-01 13:37:44 -0400

    APRIL FOOLS
    not a joke

--- server.js
+++ server.js
@@ -6,17 +6,6 @@ var Datastore = require('nedb');
 var db1 = new Datastore({filename: "data1.json", autoload: true});
 var db2 = new Datastore({filename: "data2.json", autoload: true});
 
-function updateComments(){
-
-	db2.find({}, function(err, docs) {
-		for (var i = 0; i < docs.length; i++) {
-			console.log(docs[i].name + ": " + docs[i].comment);
-			allData2.push(docs[i].name + ": " + docs[i].comment);
-		}
-	});
-}
-
-
 app.listen(3001, function () {
   console.log('Example app listening on port 3001');
 });
@@ -26,11 +15,13 @@ app.get('/project1comments', function(req, res) {
 		name: req.query.name,
 		comment: req.query.comment
 	};
-	db1.insert(dataToSave, function (err, newDocs) {
-		db1.find({}, function(err, docs) {
-			res.send(docs);
+	if ("name" in req.query){
+		db1.insert(dataToSave, function (err, newDocs) {
+			db1.find({}, function(err, docs) {
+				res.send(docs);
+			});
 		});
-	});
+	}
 });	
 
 app.get('/project2comments', function(req, res) {
@@ -38,11 +29,13 @@ app.get('/project2comments', function(req, res) {
 		name: req.query.name,
 		comment: req.query.comment
 	}
-	db2.insert(dataToSave, function (err, newDocs) {
-		db2.find({}, function(err, docs) {
-			res.send(docs);
+	if ("name" in req.query){
+		db2.insert(dataToSave, function (err, newDocs) {
+			db2.find({}, function(err, docs) {
+				res.send(docs);
+			});
 		});
-	});
+	}
 });	
 
 
